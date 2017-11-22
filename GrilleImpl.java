5541cc79733682562b1a709e8b1605ebb20f157d
cedricfeze <siyoucedric@yahoo.fr>
2017-11-22 07:14:48 +0100

    Implementation de la grille

--- GrilleImpl.java
+++ GrilleImpl.java
@@ -3,11 +3,25 @@ package sudoku;
 import java.util.Arrays;
 
 public abstract class GrilleImpl implements Grille {
+	/**
+	 * @retourne largeur/hauteur de la grille
+	 */
 	public int getDimension() {
 		int largeur = 500;
 		int  hauteur = 100;
 		return largeur / hauteur;
 	}
+	/**
+     * Affecte une valeur dans la grille
+     * 
+     * @param x       position x dans la grille
+     * @param y       position y dans la grille
+     * @param value   valeur a mettre dans la case
+     * @throw IllegalArgumentException si x ou y sont hors bornes (0-8)
+     * @throw IllegalArgumentException si la valeur est interdite aux vues des autres valeurs de la grille
+     * @throw IllegalArgumentException si value n'est pas un caractere autorise ('1',...,'9')
+     * 
+     */
 	 public  void setvalue(final int x, final int y, final char value) {
 	    	int [][] grille = new int[9][9];
 			char[] possible = new char[] {'1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f'};
@@ -26,6 +40,14 @@ public abstract class GrilleImpl implements Grille {
 				throw new IllegalArgumentException("x ou y n'est pas compris dans la borne 0-8 ");
 			}
 	    }
+	 /**
+	     * Recupere une valeur de la grille 
+	     * 
+	     * @param x position x dans la grille
+	     * @param y position y dans la grille 
+	     * @return valeur dans la case x,y
+	     * @throw IllegalArgumentException si x ou y sont hors bornes (0-8)
+	     */
 	 public final int getvalue(int x, int y){
 	    	int [][] grille = new int[9][9];
 	    	int value = 0;
@@ -39,7 +61,11 @@ public abstract class GrilleImpl implements Grille {
 			}
 			return value;
 	    }
-		
+	    /**
+	     * Test si une grille est terminée
+	     * 
+	     * @return true si la grille est complete
+	     */
 		public static boolean complete(int x, int y){
 		      if(y == 8 ) 
 		    	{ x = x + 1; y=0;
@@ -49,6 +75,15 @@ public abstract class GrilleImpl implements Grille {
 		    	}
 		    	return true;
 			}
+		/**
+	     * Test si une valeur est possible dans la grille par rapport a ce qu'elle contient déjà 
+	     *
+	     * @param x position x dans la grille 
+	     * @param y position y dans la grille
+	     * @param value valeur a mettre dans la case
+	     * @Throw IllegalArgumentException si x ou y sont hors bornes (0-8)
+	     * @Throw IllegalArgumentException si value n'est pas un caractère autorise ('1',...,'9',...)
+	     */
 		public  boolean possible(int x, int y, char value) {
 			int [][] grille = new int[9][9];
 			char[] possible = new char[] {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f'};
